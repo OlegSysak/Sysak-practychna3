c7ecc87561bfed8f59b2bc89c76ed3a1aa027312
OlegSysak <oleh.r.sysak@ukd.edu.ua>
2022-12-18 16:31:07 +0200

    PR

---
+++ pythonPRACTYCHNA3/main.py
@@ -0,0 +1,467 @@
+a1 = input()
+b1 = input()
+c1 = input()
+d1 = input()
+e1 = input()
+f1 = input()
+g1 = input()
+j1 = input()
+s = (f" \n #### \n# \n# \n ### \n    # \n    # \n ### \n")
+y = (f"\n#    # \n#    # \n #  # \n  # \n  # \n  # \n  # \n")
+a = (f" \n  ### \n #   # \n #   # \n ##### \n #   # \n #   # \n #   # \n")
+k = (f" \n #   # \n #  # \n # # \n ## \n # # \n #  # \n #   # \n" )
+o = (f" \n *** \n*   * \n*   * \n*   * \n*   * \n*   * \n *** \n")
+l = (f" \n * \n * \n * \n * \n * \n * \n ***** \n")
+e = (f" \n ***** \n * \n * \n *** \n * \n * \n ***** \n")
+d = (f" \n ####  \n #   # \n #   # \n #   # \n #   # \n #   # \n #### ")
+r = (f" \n #### \n #   # \n #   # \n #### \n # # \n #  # \n #   # ")
+n = (f" \n #    # \n #    # \n ##   # \n # #  # \n #   ## \n #    # \n #    #")
+b = (f" \n ####  \n #   # \n #   # \n ####  \n #   # \n #   # \n ####   ")
+c = (f" \n  ###  \n #   # \n # \n # \n # \n # \n # \n #   # \n  ### ")
+g = (f" \n  ### \n #   # \n # \n #  ## \n #   # \n #   # \n  ### ")
+h = (f" \n #   # \n #   # \n #   # \n ##### \n #   # \n #   # \n #   #")
+i = (f" \n ### \n  # \n  # \n  # \n  # \n  # \n ###")
+j = (f" \n ### \n  # \n  # \n  # \n  # \n  # \n  # \n##  ")
+m = (f" \n #   # \n #   # \n ## ## \n # # # \n #   # \n #   # \n #   # ")
+p = (f" \n #### \n #   # \n #   # \n #### \n # \n # \n #")
+q = (f" \n  ### \n #   # \n #   # \n #   # \n #   # \n #  # \n  ## #")
+t = (f" \n ##### \n   # \n   # \n   # \n   # \n   # \n   # ")
+u = (f" \n #   # \n #   # \n #   # \n #   # \n #   # \n #   # \n  ### ")
+w = (f" \n #   # \n #   # \n #   # \n # # # \n ## ## \n #   # \n #   # ")
+x = (f"\n #   # \n #   # \n  # # \n   #  \n  # # \n #   # \n #   #")
+z = (f" \n ##### \n     # \n    # \n   # \n  # \n # \n #####")
+f = (f"\n##### \n# \n# \n### \n# \n# \n#")
+v = (f"\n#   # \n#   # \n#   # \n#   # \n#   # \n # # \n  # ")
+i1 = 0
+while a1 == "s" or "y" or "a" or "k" or "o" or "l" or "e" or "d" or "r" or "n" or "b" or "c" or "g" or "h" or "i" or "j" or "m" or "p" or "q" or "t" or "u" or "w" or "x" or "z" or "f" or "v":
+    if a1[i1] == "s":
+        print(s)
+    if a1[i1] == "y":
+        print(y)
+    if a1[i1] == "a":
+        print(a)
+    if a1[i1] == "k":
+        print(k)
+    if a1[i1] == "o":
+        print(o)
+    if a1[i1] == "l":
+        print(l)
+    if a1[i1] == "e":
+        print(e)
+    if a1[i1] == "d":
+        print(d)
+    if a1[i1] == "r":
+        print(r)
+    if a1[i1] == "n":
+        print(n)
+    if a1[i1] == "b":
+        print(b)
+    if a1[i1] == "c":
+        print(c)
+    if a1[i1] == "g":
+        print(g)
+    if a1[i1] == "h":
+        print(h)
+    if a1[i1] == "i":
+        print(i)
+    if a1[i1] == "j":
+        print(j)
+    if a1[i1] == "m":
+        print(m)
+    if a1[i1] == "p":
+        print(p)
+    if a1[i1] == "q":
+        print(q)
+    if a1[i1] == "t":
+        print(t)
+    if a1[i1] == "u":
+        print(u)
+    if a1[i1] == "w":
+        print(w)
+    if a1[i1] == "x":
+        print(x)
+    if a1[i1] == "z":
+        print(z)
+    if a1[i1] == "f":
+        print(f)
+    if a1[i1] == "v":
+        print(v)
+    break
+while b1 == "s" or "y" or "a" or "k" or "o" or "l" or "e" or "d" or "r" or "n" or "b" or "c" or "g" or "h" or "i" or "j" or "m" or "p" or "q" or "t" or "u" or "w" or "x" or "z":
+    if b1[i1] == "s":
+        print(s)
+    if b1[i1] == "y":
+        print(y)
+    if b1[i1] == "a":
+        print(a)
+    if b1[i1] == "k":
+        print(k)
+    if b1[i1] == "o":
+        print(o)
+    if b1[i1] == "l":
+        print(l)
+    if b1[i1] == "e":
+        print(e)
+    if b1[i1] == "d":
+        print(d)
+    if b1[i1] == "r":
+        print(r)
+    if b1[i1] == "n":
+        print(n)
+    if b1[i1] == "b":
+        print(b)
+    if b1[i1] == "c":
+        print(c)
+    if b1[i1] == "g":
+        print(g)
+    if b1[i1] == "h":
+        print(h)
+    if b1[i1] == "i":
+        print(i)
+    if b1[i1] == "j":
+        print(j)
+    if b1[i1] == "m":
+        print(m)
+    if b1[i1] == "p":
+        print(p)
+    if b1[i1] == "q":
+        print(q)
+    if b1[i1] == "t":
+        print(t)
+    if b1[i1] == "u":
+        print(u)
+    if b1[i1] == "w":
+        print(w)
+    if b1[i1] == "x":
+        print(x)
+    if b1[i1] == "z":
+        print(z)
+    if b1[i1] == "f":
+        print(f)
+    if b1[i1] == "v":
+        print(v)
+    break
+while c1 == "s" or "y" or "a" or "k" or "o" or "l" or "e" or "d" or "r" or "n" or "b" or "c" or "g" or "h" or "i" or "j" or "m" or "p" or "q" or "t" or "u" or "w" or "x" or "z" or "f" or "v":
+    if c1[i1] == "s":
+        print(s)
+    if c1[i1] == "y":
+        print(y)
+    if c1[i1] == "a":
+        print(a)
+    if c1[i1] == "k":
+        print(k)
+    if c1[i1] == "o":
+        print(o)
+    if c1[i1] == "l":
+        print(l)
+    if c1[i1] == "e":
+        print(e)
+    if c1[i1] == "d":
+        print(d)
+    if c1[i1] == "r":
+        print(r)
+    if c1[i1] == "n":
+        print(n)
+    if c1[i1] == "b":
+        print(b)
+    if c1[i1] == "c":
+        print(c)
+    if c1[i1] == "g":
+        print(g)
+    if c1[i1] == "h":
+        print(h)
+    if c1[i1] == "i":
+        print(i)
+    if c1[i1] == "j":
+        print(j)
+    if c1[i1] == "m":
+        print(m)
+    if c1[i1] == "p":
+        print(p)
+    if c1[i1] == "q":
+        print(q)
+    if c1[i1] == "t":
+        print(t)
+    if c1[i1] == "u":
+        print(u)
+    if c1[i1] == "w":
+        print(w)
+    if c1[i1] == "x":
+        print(x)
+    if c1[i1] == "z":
+        print(z)
+    if c1[i1] == "f":
+        print(f)
+    if c1[i1] == "v":
+        print(v)
+    break
+while d1 == "s" or "y" or "a" or "k" or "o" or "l" or "e" or "d" or "r" or "n" or "b" or "c" or "g" or "h" or "i" or "j" or "m" or "p" or "q" or "t" or "u" or "w" or "x" or "z":
+    if d1[i1] == "s":
+        print(s)
+    if d1[i1] == "y":
+        print(y)
+    if d1[i1] == "a":
+        print(a)
+    if d1[i1] == "k":
+        print(k)
+    if d1[i1] == "o":
+        print(o)
+    if d1[i1] == "l":
+        print(l)
+    if d1[i1] == "e":
+        print(e)
+    if d1[i1] == "d":
+        print(d)
+    if d1[i1] == "r":
+        print(r)
+    if d1[i1] == "n":
+        print(n)
+    if d1[i1] == "b":
+        print(b)
+    if d1[i1] == "c":
+        print(c)
+    if d1[i1] == "g":
+        print(g)
+    if d1[i1] == "h":
+        print(h)
+    if d1[i1] == "i":
+        print(i)
+    if d1[i1] == "j":
+        print(j)
+    if d1[i1] == "m":
+        print(m)
+    if d1[i1] == "p":
+        print(p)
+    if d1[i1] == "q":
+        print(q)
+    if d1[i1] == "t":
+        print(t)
+    if d1[i1] == "u":
+        print(u)
+    if d1[i1] == "w":
+        print(w)
+    if d1[i1] == "x":
+        print(x)
+    if d1[i1] == "z":
+        print(z)
+    if d1[i1] == "f":
+        print(f)
+    if d1[i1] == "v":
+        print(v)
+    break
+while e1 == "s" or "y" or "a" or "k" or "o" or "l" or "e" or "d" or "r" or "n" or "b" or "c" or "g" or "h" or "i" or "j" or "m" or "p" or "q" or "t" or "u" or "w" or "x" or "z" or "f" or "v":
+    if e1[i1] == "s":
+        print(s)
+    if e1[i1] == "y":
+        print(y)
+    if e1[i1] == "a":
+        print(a)
+    if e1[i1] == "k":
+        print(k)
+    if e1[i1] == "o":
+        print(o)
+    if e1[i1] == "l":
+        print(l)
+    if e1[i1] == "e":
+        print(e)
+    if e1[i1] == "d":
+        print(d)
+    if e1[i1] == "r":
+        print(r)
+    if e1[i1] == "n":
+        print(n)
+    if e1[i1] == "b":
+        print(b)
+    if e1[i1] == "c":
+        print(c)
+    if e1[i1] == "g":
+        print(g)
+    if e1[i1] == "h":
+        print(h)
+    if e1[i1] == "i":
+        print(i)
+    if e1[i1] == "j":
+        print(j)
+    if e1[i1] == "m":
+        print(m)
+    if e1[i1] == "p":
+        print(p)
+    if e1[i1] == "q":
+        print(q)
+    if e1[i1] == "t":
+        print(t)
+    if e1[i1] == "u":
+        print(u)
+    if e1[i1] == "w":
+        print(w)
+    if e1[i1] == "x":
+        print(x)
+    if e1[i1] == "z":
+        print(z)
+    if e1[i1] == "f":
+        print(f)
+    if e1[i1] == "v":
+        print(v)
+    break
+while f1 == "s" or "y" or "a" or "k" or "o" or "l" or "e" or "d" or "r" or "n" or "b" or "c" or "g" or "h" or "i" or "j" or "m" or "p" or "q" or "t" or "u" or "w" or "x" or "z" or "f" or "v":
+    if f1[i1] == "s":
+        print(s)
+    if f1[i1] == "y":
+        print(y)
+    if f1[i1] == "a":
+        print(a)
+    if f1[i1] == "k":
+        print(k)
+    if f1[i1] == "o":
+        print(o)
+    if f1[i1] == "l":
+        print(l)
+    if f1[i1] == "e":
+        print(e)
+    if f1[i1] == "d":
+        print(d)
+    if f1[i1] == "r":
+        print(r)
+    if f1[i1] == "n":
+        print(n)
+    if f1[i1] == "b":
+        print(b)
+    if f1[i1] == "c":
+        print(c)
+    if f1[i1] == "g":
+        print(g)
+    if f1[i1] == "h":
+        print(h)
+    if f1[i1] == "i":
+        print(i)
+    if f1[i1] == "j":
+        print(j)
+    if f1[i1] == "m":
+        print(m)
+    if f1[i1] == "p":
+        print(p)
+    if f1[i1] == "q":
+        print(q)
+    if f1[i1] == "t":
+        print(t)
+    if f1[i1] == "u":
+        print(u)
+    if f1[i1] == "w":
+        print(w)
+    if f1[i1] == "x":
+        print(x)
+    if f1[i1] == "z":
+        print(z)
+    if f1[i1] == "f":
+        print(f)
+    if f1[i1] == "v":
+        print(v)
+    break
+while g1 == "s" or "y" or "a" or "k" or "o" or "l" or "e" or "d" or "r" or "n" or "b" or "c" or "g" or "h" or "i" or "j" or "m" or "p" or "q" or "t" or "u" or "w" or "x" or "z" or "f" or "v":
+    if g1[i1] == "s":
+        print(s)
+    if g1[i1] == "y":
+        print(y)
+    if g1[i1] == "a":
+        print(a)
+    if g1[i1] == "k":
+        print(k)
+    if g1[i1] == "o":
+        print(o)
+    if g1[i1] == "l":
+        print(l)
+    if g1[i1] == "e":
+        print(e)
+    if g1[i1] == "d":
+        print(d)
+    if g1[i1] == "r":
+        print(r)
+    if g1[i1] == "n":
+        print(n)
+    if g1[i1] == "b":
+        print(b)
+    if g1[i1] == "c":
+        print(c)
+    if g1[i1] == "g":
+        print(g)
+    if g1[i1] == "h":
+        print(h)
+    if g1[i1] == "i":
+        print(i)
+    if g1[i1] == "j":
+        print(j)
+    if g1[i1] == "m":
+        print(m)
+    if g1[i1] == "p":
+        print(p)
+    if g1[i1] == "q":
+        print(q)
+    if g1[i1] == "t":
+        print(t)
+    if g1[i1] == "u":
+        print(u)
+    if g1[i1] == "w":
+        print(w)
+    if g1[i1] == "x":
+        print(x)
+    if g1[i1] == "z":
+        print(z)
+    if g1[i1] == "f":
+        print(f)
+    if g1[i1] == "v":
+        print(v)
+    break
+while j1 == "s" or "y" or "a" or "k" or "o" or "l" or "e" or "d" or "r" or "n" or "b" or "c" or "g" or "h" or "i" or "j" or "m" or "p" or "q" or "t" or "u" or "w" or "x" or "z" or "f" or "v":
+    if j1[i1] == "s":
+        print(s)
+    if j1[i1] == "y":
+        print(y)
+    if j1[i1] == "a":
+        print(a)
+    if j1[i1] == "k":
+        print(k)
+    if j1[i1] == "o":
+        print(o)
+    if j1[i1] == "l":
+        print(l)
+    if j1[i1] == "e":
+        print(e)
+    if j1[i1] == "d":
+        print(d)
+    if j1[i1] == "r":
+        print(r)
+    if j1[i1] == "n":
+        print(n)
+    if j1[i1] == "b":
+        print(b)
+    if j1[i1] == "c":
+        print(c)
+    if j1[i1] == "g":
+        print(g)
+    if j1[i1] == "h":
+        print(h)
+    if j1[i1] == "i":
+        print(i)
+    if j1[i1] == "j":
+        print(j)
+    if j1[i1] == "m":
+        print(m)
+    if j1[i1] == "p":
+        print(p)
+    if j1[i1] == "q":
+        print(q)
+    if j1[i1] == "t":
+        print(t)
+    if j1[i1] == "u":
+        print(u)
+    if j1[i1] == "w":
+        print(w)
+    if j1[i1] == "x":
+        print(x)
+    if j1[i1] == "z":
+        print(z)
+    if j1[i1] == "f":
+        print(f)
+    if j1[i1] == "v":
+        print(v)
+    break
